baa3975696c83142e5a7dacebfaf65ea643108e1
gstfnk <gabrysia.stefaniak@gmail.com>
2024-10-22 22:47:02 +0200

    change to type of mapping

--- src/main/java/sggw/wzim/czasnawypad/controller/TestApiController.java
+++ src/main/java/sggw/wzim/czasnawypad/controller/TestApiController.java
@@ -1,6 +1,7 @@
 package sggw.wzim.czasnawypad.controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class TestApiController {
 
-    @RequestMapping("/test")
+    @GetMapping("/test")
     public String helloWorld() {
         return "Hello World from Api Controller! \uD83D\uDC31";
     }
