dbc950dccf13ef89a6de416b488999fa8c8e0f20
Szymon Wolanin <szymon.wolanin.poczta@gmail.com>
2024-11-20 21:43:35 +0100

    Dodano filtrowanie po ocenie atrakcji

--- src/main/java/sggw/wzim/czasnawypad/controller/AttractionRatingController.java
+++ src/main/java/sggw/wzim/czasnawypad/controller/AttractionRatingController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
@@ -40,5 +41,10 @@ public class AttractionRatingController {
                                              @AuthenticationPrincipal User user,
                                              @RequestBody CreateAttractionRatingDTO dto) {
         return ratingService.updateRating(id, user, dto);
+    }
+
+    @GetMapping("/filter")
+    public List<Attraction> filterAttractionsByRating(@RequestParam double minRating) {
+        return ratingService.findAttractionsByMinimumAverageRating(minRating);
     }
 }
