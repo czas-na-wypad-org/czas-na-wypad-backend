506cb1c64d1fba96443e8c4ce0fc8b72be981e48
wkingakos <s224921@sggw.edu.pl>
2024-11-22 21:48:30 +0100

    Add files via upload

---
+++ src/main/java/sggw/wzim/czasnawypad/repository/FavouriteAttractionRepository.java
@@ -0,0 +1,16 @@
+package sggw.wzim.czasnawypad.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import sggw.wzim.czasnawypad.db.FavouriteAttraction;
+
+@Repository
+public interface FavouriteAttractionRepository extends JpaRepository<FavouriteAttraction, Long> {
+    List<FavouriteAttraction> findByUser(User user);
+    List<FavouriteAttraction> findByAttractionId(Long attractionId);
+    
+}
