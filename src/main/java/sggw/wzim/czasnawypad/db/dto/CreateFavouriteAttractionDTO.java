221a7160f57417559916ed7fea134052f89ef02b
wkingakos <s224921@sggw.edu.pl>
2024-11-22 21:46:45 +0100

    Add files via upload

---
+++ src/main/java/sggw/wzim/czasnawypad/db/dto/CreateFavouriteAttractionDTO.java
@@ -0,0 +1,12 @@
+package sggw.wzim.czasnawypad.db.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class CreateFavouriteAttractionDTO {
+	@NotNull
+	private Long attractionId;
+	@NotNull
+	private Long userId;
+}	
---
+++ src/main/java/sggw/wzim/czasnawypad/db/dto/FavouriteAttractionDTO.java
@@ -0,0 +1,12 @@
+package sggw.wzim.czasnawypad.db.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class FavouriteAttractionDTO {
+	private Long id;
+	private Long attractionId;
+	private Long userId;
+}	
