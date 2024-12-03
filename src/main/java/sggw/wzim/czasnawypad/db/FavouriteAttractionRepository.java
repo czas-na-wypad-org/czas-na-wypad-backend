3b6fe26b993250ba1123ed8488f185bb1d9d29f7
Weronika <s224921@sggw.edu.pl>
2024-12-03 22:33:15 +0100

    FavouriteAttractionRepository fix3

--- src/main/java/sggw/wzim/czasnawypad/db/FavouriteAttractionRepository.java
+++ src/main/java/sggw/wzim/czasnawypad/db/FavouriteAttractionRepository.java
@@ -4,15 +4,14 @@ import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import sggw.wzim.czasnawypad.db.entity.FavouriteAttraction;
-import sggw.wzim.czasnawypad.db.entity.User;
 
 import java.util.List;
 
 @Hidden
 @Repository
-public interface FavouriteAttractionRepository extends JpaRepository<FavouriteAttraction, User> {
+public interface FavouriteAttractionRepository extends JpaRepository<FavouriteAttraction, Integer> {
 
-    List<FavouriteAttraction> findByUser(User user);
+    List<FavouriteAttraction> findByUserId(Integer userId);
     List<FavouriteAttraction> findByAttractionId(Integer attractionId);
 
 
