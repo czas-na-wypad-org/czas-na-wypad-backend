5c0b047e2b711b906afa58a97fb19eac26024f0a
Weronika <s224921@sggw.edu.pl>
2024-12-03 21:55:08 +0100

    Update FavouriteAttractionDTO and CreateFavouriteAttractionDTO final2

--- src/main/java/sggw/wzim/czasnawypad/db/dto/CreateFavouriteAttractionDTO.java
+++ src/main/java/sggw/wzim/czasnawypad/db/dto/CreateFavouriteAttractionDTO.java
@@ -10,4 +10,4 @@ public class CreateFavouriteAttractionDTO {
 	@NotNull
 	private Integer userId;
 
-}	
+}
--- src/main/java/sggw/wzim/czasnawypad/db/dto/FavouriteAttractionDTO.java
+++ src/main/java/sggw/wzim/czasnawypad/db/dto/FavouriteAttractionDTO.java
@@ -10,4 +10,4 @@ public class FavouriteAttractionDTO {
 	private Integer attractionId;
 	private Integer userId;
 
-}	
+}
