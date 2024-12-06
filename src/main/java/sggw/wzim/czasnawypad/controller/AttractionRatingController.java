a0a25b1685434679b9ea91d5638889ea8f74d3f0
Szymon Wolanin <szymon.wolanin.poczta@gmail.com>
2024-12-06 14:51:53 +0100

    poprawki do endpointow

--- src/main/java/sggw/wzim/czasnawypad/controller/AttractionRatingController.java
+++ src/main/java/sggw/wzim/czasnawypad/controller/AttractionRatingController.java
@@ -2,6 +2,7 @@ package sggw.wzim.czasnawypad.controller;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import sggw.wzim.czasnawypad.db.dto.AttractionAverageRatingDTO;
 import sggw.wzim.czasnawypad.db.dto.AttractionRatingDTO;
 import sggw.wzim.czasnawypad.db.dto.CreateAttractionRatingDTO;
 import sggw.wzim.czasnawypad.db.entity.Attraction;
@@ -21,37 +23,41 @@ import sggw.wzim.czasnawypad.db.entity.User;
 import sggw.wzim.czasnawypad.service.AttractionRatingService;
 
 @RestController
-@RequestMapping("/api/ratings")
+@RequestMapping("/ratings")
 @RequiredArgsConstructor
 public class AttractionRatingController {
 
     private final AttractionRatingService ratingService;
 
     @GetMapping("/attraction/{attractionId}")
-    public List<AttractionRatingDTO> getRatingsForAttraction(@PathVariable Integer attractionId) {
-        return ratingService.getRatingsForAttraction(attractionId);
+    public ResponseEntity<?> getRatingsForAttraction(@PathVariable Integer attractionId) {
+        List<AttractionRatingDTO> attractionRatings = ratingService.getRatingsForAttraction(attractionId);
+        return ResponseEntity.ok(attractionRatings);
     }
     
     @GetMapping
-    public List<AttractionRatingDTO> getUserRatings(@AuthenticationPrincipal User user) {
-        return ratingService.getRatingsByUser(user);
+    public ResponseEntity<?> getUserRatings(Integer userId) {
+        List<AttractionRatingDTO> userRatings = ratingService.getRatingsByUser(userId);
+        return ResponseEntity.ok(userRatings);
     }
 
-     @PostMapping
-    public AttractionRatingDTO addRating(@AuthenticationPrincipal User user,
-                                         @Valid @RequestBody CreateAttractionRatingDTO dto) {
-        return ratingService.addRating(user, dto);
+    @PostMapping
+    public ResponseEntity<?> addRating(@Valid @RequestBody CreateAttractionRatingDTO dto) {
+        AttractionRatingDTO createdRatingDTO = ratingService.addRating(dto);
+        return ResponseEntity.ok().body(createdRatingDTO);
     }
 
     @PutMapping("/{id}")
-    public AttractionRatingDTO updateRating(@PathVariable Integer id,
-                                             @AuthenticationPrincipal User user,
+    public ResponseEntity<?> updateRating(@PathVariable Integer id,
+                                          Integer userId,
                                              @RequestBody CreateAttractionRatingDTO dto) {
-        return ratingService.updateRating(id, user, dto);
+        AttractionRatingDTO updatedRatingDTO = ratingService.updateRating(id, userId, dto);
+        return ResponseEntity.ok(updatedRatingDTO);
     }
 
     @GetMapping("/filter")
-    public List<Attraction> filterAttractionsByRating(@RequestParam double minRating) {
-        return ratingService.findAttractionsByMinimumAverageRating(minRating);
+    public ResponseEntity<?> filterAttractionsByRating(@RequestParam double minRating) {
+        List<AttractionAverageRatingDTO> attractionsByRating = ratingService.findAttractionsByMinimumAverageRating(minRating);
+        return ResponseEntity.ok(attractionsByRating);
     }
 }
--- src/main/java/sggw/wzim/czasnawypad/db/dto/CreateAttractionRatingDTO.java
+++ src/main/java/sggw/wzim/czasnawypad/db/dto/CreateAttractionRatingDTO.java
@@ -5,13 +5,19 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 @Data
 public class CreateAttractionRatingDTO {
     @NotNull
-    private int attractionId;
+    private Integer attractionId;
+    @NotNull
+    private Integer userId;
     @NotNull
     @Min(1)
     @Max(5)
-    private Byte rating;
+    private Integer rating;
     private String notes;
+    private Date date;
 }
